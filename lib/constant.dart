a55b81c2a3b194f8a6e716d5b6b7e9552e26d106
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-07 15:49:20 +0100

    💉 added : SVG dependency

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 //This folder contains fixed / default stuff we might use across our App
 
@@ -7,5 +5,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF635BEB);
 const Color logoRed = Color(0xFFD90000);
 const Color logoBlue = Color(0xFf0063DB);
-const Color lightGray = Color(0xFF3B3B3B);
+const Color lightGray = Color(0xFFF6F6F6);
+// const Color lightGray = Color(0xFF3B3B3B);
+
 const Color mainPurple = Color(0xFF635BEB);
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,94 +1,101 @@
-import 'package:flutter/material.dart';
-import '../../constant.dart';
+// import 'package:flutter/material.dart';
+// import 'package:zapp/screens/sign_up/sign_up.dart';
+// import '../../constant.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+// class LoginScreen extends StatelessWidget {
+//   const LoginScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Spacer(flex: 2),
-            SizedBox(
-              child: Image.asset("assets/images/logo2.png"),
-            ),
-            const Spacer(),
-            const Text(
-              "Welcome to Jejelove Health !",
-              style: TextStyle(
-                color: kPrimaryColor,
-                //fontFamily: 'Montserrat',
-                fontWeight: FontWeight.bold,
-                fontSize: 22.0,
-                // letterSpacing: 2.0,
-              ),
-            ),
-            const Spacer(),
-            const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    backgroundColor: kPrimaryColor,
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    "Create Account",
-                    // style: TextStyle(
-                    //   fontSize: getProportionateScreenHeight(18),
-                    // ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: GestureDetector(
-                  child: Container(
-                    // height: 50,
-                    // width: 200,
-                    decoration: BoxDecoration(
-                      // color: kPrimaryColor,
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Spacer(flex: 2),
+//             SizedBox(
+//               child: Image.asset("assets/images/logo2.png"),
+//             ),
+//             const Spacer(),
+//             const Text(
+//               "Welcome to Jejelove Health !",
+//               style: TextStyle(
+//                 color: kPrimaryColor,
+//                 fontFamily: 'Righteous',
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 22.0,
+//                 // letterSpacing: 2.0,
+//               ),
+//             ),
+//             const Spacer(),
+//             const SizedBox(height: 15),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 50),
+//               child: SizedBox(
+//                 width: double.infinity,
+//                 height: 50,
+//                 child: ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(20),
+//                     ),
+//                     backgroundColor: kPrimaryColor,
+//                   ),
+//                   onPressed: () {
+//                     Navigator.of(context).push(
+//                       MaterialPageRoute(
+//                         builder: (context) => const SignUpScreen(),
+//                       ),
+//                     );
+//                   },
+//                   child: const Text(
+//                     "Create Account",
+//                     // style: TextStyle(
 
-                      border: Border.all(
-                        color: kPrimaryColor,
-                        // width: 5.0,
-                      ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50.0),
-                      ),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(color: kPrimaryColor
-                            //fontFamily: 'Montserrat',
-                            //fontSize: 18.0,
-                            // letterSpacing: 2.0,
-                            ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//                     // ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 15),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 50),
+//               child: SizedBox(
+//                 width: double.infinity,
+//                 height: 50,
+//                 child: GestureDetector(
+//                   child: Container(
+//                     // height: 50,
+//                     // width: 200,
+//                     decoration: BoxDecoration(
+//                       // color: kPrimaryColor,
+
+//                       border: Border.all(
+//                         color: kPrimaryColor,
+//                         // width: 5.0,
+//                       ),
+//                       borderRadius: const BorderRadius.all(
+//                         Radius.circular(50.0),
+//                       ),
+//                     ),
+//                     child: const Center(
+//                       child: Text(
+//                         'Sign In',
+//                         style: TextStyle(color: kPrimaryColor
+//                             //fontFamily: 'Montserrat',
+//                             //fontSize: 18.0,
+//                             // letterSpacing: 2.0,
+//                             ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
