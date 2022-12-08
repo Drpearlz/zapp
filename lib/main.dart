ca64388a43b1b6111dba289a66bdd899a32ec525
Hammed Ibraheem Omowunmi <67191200+Scientist265@users.noreply.github.com>
2022-12-08 03:35:22 +0100

    client screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:zapp/screens/clientScreens/client_screen.dart';
 import 'package:zapp/screens/intro/onboarding.dart';
 import 'package:zapp/screens/intro/welcome_screen.dart';
 import 'package:zapp/screens/login/login_screen.dart';
 import 'package:zapp/screens/sign_up/sign_up.dart';
 import 'package:zapp/screens/splash.dart';
+
+import 'utils/bottombar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,20 +18,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: SplashScreen
 
       // for those that wwant to check each screens separately,
       // just edit the '/' in the initial route to any of the routes e.g '/login'
-      initialRoute: '/',
-      // This is the routes for the different screens so far
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/sign_up': (context) => const SignUpScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
-        '/onboarding': (context) => const OnboardingPage(),
-        '/login': (context) => const LoginScreen(),
-      },
+      // initialRoute: '/',
+      // // This is the routes for the different screens so far
+      // routes: {
+      //   '/': (context) => const ClientScreen(),
+      //   '/sign_up': (context) => const SignUpScreen(),
+      //   '/welcome': (context) => const WelcomeScreen(),
+      //   '/onboarding': (context) => const OnboardingPage(),
+      //   '/login': (context) => const LoginScreen(),
+      // },
+      home: BottomBar(),
     );
   }
 }
