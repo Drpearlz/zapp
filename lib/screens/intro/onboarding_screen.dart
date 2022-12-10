343de31bc7b5d587431d464da7dd05d132154c91
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-12-10 10:45:08 +0100

    Add automatic scrolling in onboarding screens

--- lib/screens/intro/onboarding_screen.dart
+++ lib/screens/intro/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:zapp/utils/onboarding_screen_util.dart';
 
@@ -9,59 +10,80 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  final PageController _controller = PageController();
+  final PageController pageController = PageController();
+  int currentIndex = 0;
+  var currentTabs = [
+
+    OnboardingScreenUtil(
+      backgroundImg: 'assets/images/onboarding_1.png',
+      onBoardMsgHeading: 'Skip Long Wait Times',
+      onBoardMsgBody:
+        'Skip long wait times, by booking ahead\nto see a health professional in Nigeria.',
+    ),
+    OnboardingScreenUtil(
+      backgroundImg: 'assets/images/onboarding_2.png',
+      onBoardMsgHeading: 'Help on-the-go',
+      onBoardMsgBody:
+        'Chat with licensed professionals\non the go, via mobile',
+    ),
+    OnboardingScreenUtil(
+      backgroundImg: 'assets/images/onboarding_3.png',
+      onBoardMsgHeading: 'Thousands of Doctors',
+      onBoardMsgBody:
+        'Connect with the best doctors and\nhealth care workers in the country',
+    ),
+    OnboardingScreenUtil(
+      backgroundImg: 'assets/images/onboarding_4.png',
+      onBoardMsgHeading: 'Access to Emergency Services',
+      onBoardMsgBody:
+        'Connect easily with thousands of licensed\n\t\t\t\t\t\t\t\thealth professionals in your areas',
+      logoType: 'assets/images/logo_non_trans.png',
+    ),
+    OnboardingScreenUtil(
+      backgroundImg: 'assets/images/onboarding_5.png',
+      onBoardMsgHeading: 'Access Your Health Records',
+      onBoardMsgBody:
+        '    Get access and keep track of all your\n health reports and prescriptions, at your\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t fingertips',
+      buttonText: 'Get Started',
+    ),
+
+  ];
 
   @override
-/*
   void initState() {
-    controller.addListener(() {
-      setState(() {
-        currentPage = _controller.page!.toDouble();
-        print(currentPage);
-      });
-    });
     super.initState();
+    Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (currentIndex < 2) {
+        currentIndex++;
+        pageController.animateToPage(
+          currentIndex,
+          duration: Duration(milliseconds: 350),
+      curve: Curves.easeIn,
+        );
+      } else {
+        currentIndex = 2;
+      }
+    });
   }
-*/
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        controller: _controller,
+        controller: pageController,
         scrollDirection: Axis.horizontal,
-        children: const [
-          OnboardingScreenUtil(
-            backgroundImg: 'assets/images/onboarding_1.png',
-            onBoardMsgHeading: 'Skip Long Wait Times',
-            onBoardMsgBody:
-                'Skip long wait times, by booking ahead\nto see a health professional in Nigeria.',
-          ),
-          OnboardingScreenUtil(
-            backgroundImg: 'assets/images/onboarding_2.png',
-            onBoardMsgHeading: 'Help on-the-go',
-            onBoardMsgBody:
-                'Chat with licensed professionals\non the go, via mobile',
-          ),
-          OnboardingScreenUtil(
-            backgroundImg: 'assets/images/onboarding_3.png',
-            onBoardMsgHeading: 'Thousands of Doctors',
-            onBoardMsgBody:
-                'Connect with the best doctors and\nhealth care workers in the country',
-          ),
-          OnboardingScreenUtil(
-            backgroundImg: 'assets/images/onboarding_4.png',
-            onBoardMsgHeading: 'Access to Emergency Services',
-            onBoardMsgBody:
-                'Connect easily with thousands of licensed\n\t\t\t\t\t\t\t\thealth professionals in your areas',
-            logoType: 'assets/images/logo_non_trans.png',
-          ),
-          OnboardingScreenUtil(
-            backgroundImg: 'assets/images/onboarding_5.png',
-            onBoardMsgHeading: 'Access Your Health Records',
-            onBoardMsgBody:
-                '    Get access and keep track of all your\n health reports and prescriptions, at your\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t fingertips',
-            buttonText: 'Get Started',
-          ),
-        ],
+        children: currentTabs,
+        onPageChanged: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
     );
   }
