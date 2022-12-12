17ef67d60d5e83adcd1ad0d24a912fff522a3897
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-12-12 05:02:15 +0100

    Make button clickable and remove auto scroll

--- lib/utils/onboarding_screen_util.dart
+++ lib/utils/onboarding_screen_util.dart
@@ -3,10 +3,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:zapp/constant.dart';
 import 'package:zapp/screens/intro/welcome_screen.dart';
 
-class OnboardingScreenUtil extends StatelessWidget {
+class OnboardingScreenUtil extends StatefulWidget {
   final String backgroundImg;
   final String onBoardMsgHeading;
   final String onBoardMsgBody;
+  final PageController controller; //changed here
   final String nextMsg;
   final String buttonText;
   final String logoType;
@@ -16,10 +17,16 @@ class OnboardingScreenUtil extends StatelessWidget {
       required this.backgroundImg,
       required this.onBoardMsgHeading,
       required this.onBoardMsgBody,
+      required this.controller, //changed here
       this.nextMsg = 'Skip',
       this.buttonText = 'Next',
       this.logoType = 'assets/images/logo_transparent.png'});
 
+  @override
+  State<OnboardingScreenUtil> createState() => _OnboardingScreenUtilState();
+}
+
+class _OnboardingScreenUtilState extends State<OnboardingScreenUtil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +36,7 @@ class OnboardingScreenUtil extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(backgroundImg),
+                image: AssetImage(widget.backgroundImg),
                 fit: BoxFit.fill,
               ),
             ),
@@ -58,14 +65,14 @@ class OnboardingScreenUtil extends StatelessWidget {
                   left: 259.87,
                 ),
                 child: Image.asset(
-                  logoType,
+                  widget.logoType,
                   width: 80.0,
                   height: 70.31,
                 ),
               ),
               const Spacer(),
               Text(
-                onBoardMsgHeading,
+                widget.onBoardMsgHeading,
                 style: const TextStyle(
                   color: Colors.white,
                   fontSize: 24,
@@ -77,7 +84,7 @@ class OnboardingScreenUtil extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                onBoardMsgBody,
+                widget.onBoardMsgBody,
                 style: const TextStyle(
                   color: Colors.white,
                   fontSize: 14,
@@ -95,7 +102,7 @@ class OnboardingScreenUtil extends StatelessWidget {
                   );
                 },
                 child: Text(
-                  nextMsg,
+                  widget.nextMsg,
                   style: const TextStyle(
                     color: Colors.white,
                     fontSize: 14,
@@ -123,54 +130,75 @@ class OnboardingScreenUtil extends StatelessWidget {
                             decoration: BoxDecoration(
                               color: Colors.white.withOpacity(0.5),
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
+                                  const BorderRadius.all(Radius.circular(20)),
                             ),
                           ),
-                          const SizedBox(width: 3,),
+                          const SizedBox(
+                            width: 3,
+                          ),
                           Container(
                             height: 5.0,
                             width: 22.0,
                             decoration: BoxDecoration(
                               color: Colors.white.withOpacity(0.5),
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
+                                  const BorderRadius.all(Radius.circular(20)),
                             ),
                           ),
-                          const SizedBox(width: 3,),
+                          const SizedBox(
+                            width: 3,
+                          ),
                           Container(
                             height: 5.0,
                             width: 22.0,
                             decoration: BoxDecoration(
                               color: Colors.white.withOpacity(0.5),
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
+                                  const BorderRadius.all(Radius.circular(20)),
                             ),
                           ),
-                          const SizedBox(width: 3,),
+                          const SizedBox(
+                            width: 3,
+                          ),
                           Container(
                             height: 5.0,
                             width: 22.0,
                             decoration: BoxDecoration(
                               color: Colors.white.withOpacity(0.5),
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
+                                  const BorderRadius.all(Radius.circular(20)),
                             ),
                           ),
-                          const SizedBox(width: 3,),
+                          const SizedBox(
+                            width: 3,
+                          ),
                           Container(
                             height: 5.0,
                             width: 22.0,
                             decoration: BoxDecoration(
                               color: Colors.white.withOpacity(0.5),
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
+                                  const BorderRadius.all(Radius.circular(20)),
                             ),
                           ),
                         ],
                       ),
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        if (widget.buttonText == 'Next') {
+                          widget.controller.nextPage(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                        } else {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const WelcomeScreen(),
+                            ),
+                          );
+                        }
+                      },
                       style: TextButton.styleFrom(
                         backgroundColor: Colors.white,
                       ),
@@ -181,7 +209,7 @@ class OnboardingScreenUtil extends StatelessWidget {
                         child: Row(
                           children: [
                             Text(
-                              buttonText,
+                              widget.buttonText,
                               style: const TextStyle(
                                 color: kPrimaryColor, //Color(0xFF635BEB),
                                 fontWeight: FontWeight.w700,
