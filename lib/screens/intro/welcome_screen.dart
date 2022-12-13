74cbda7b46002f84825d44b2c40031bc90899887
mrrapp <rappjeremiahn@gmail.com>
2022-12-13 11:56:36 +0100

    welcome screen, bottom overflow ressolved

--- lib/screens/intro/welcome_screen.dart
+++ lib/screens/intro/welcome_screen.dart
@@ -21,103 +21,105 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(height: 150),
-              // Logo
-              Stack(
-                children: [
-                  Center(child: Image.asset('assets/logo/flaked-logo.png')),
-                  SizedBox(height: 5),
-                  Positioned(
-                    left: 145,
-                    bottom: 30,
-                    child: Text(
-                      'Jejelove Health',
-                      style: GoogleFonts.righteous(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                          color: mainPurple),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 155),
-
-              // Welcome to Jejelove Health!
-              Text(
-                'Welcome to Jejelove Health!',
-                style: GoogleFonts.righteous(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 24,
-                    color: mainPurple),
-              ),
-              SizedBox(height: 105.1),
-
-              // Buttons
-              Column(
-                children: [
-                  // Create account button
-                  GestureDetector(
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignUpScreen(),
-                        )),
-                    child: Container(
-                      width: 366,
-                      height: 52.97,
-                      decoration: BoxDecoration(
-                          color: kPrimaryColor,
-                          borderRadius: BorderRadius.circular(30.82)),
-                      child: Center(
-                        child: Text(
-                          'Create Account',
-                          style: TextStyle(
-                              fontFamily: 'Source Sans Pro',
-                              color: white,
-                              fontSize: 17.34,
-                              fontWeight: FontWeight.w600),
-                        ),
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(height: 150),
+                // Logo
+                Stack(
+                  children: [
+                    Center(child: Image.asset('assets/logo/flaked-logo.png')),
+                    SizedBox(height: 5),
+                    Positioned(
+                      left: 145,
+                      bottom: 30,
+                      child: Text(
+                        'Jejelove Health',
+                        style: GoogleFonts.righteous(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: mainPurple),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 25.03),
+                  ],
+                ),
+                SizedBox(height: 155),
 
-                  //Sign in button
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 58.0),
-                    child: GestureDetector(
+                // Welcome to Jejelove Health!
+                Text(
+                  'Welcome to Jejelove Health!',
+                  style: GoogleFonts.righteous(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 24,
+                      color: mainPurple),
+                ),
+                SizedBox(height: 105.1),
+
+                // Buttons
+                Column(
+                  children: [
+                    // Create account button
+                    GestureDetector(
                       onTap: () => Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => LoginScreen(),
+                            builder: (context) => SignUpScreen(),
                           )),
                       child: Container(
                         width: 366,
                         height: 52.97,
                         decoration: BoxDecoration(
-                            border: Border.all(
-                              color: mainPurple,
-                            ),
+                            color: kPrimaryColor,
                             borderRadius: BorderRadius.circular(30.82)),
                         child: Center(
                           child: Text(
-                            'Sign In',
+                            'Create Account',
                             style: TextStyle(
                                 fontFamily: 'Source Sans Pro',
-                                color: mainPurple,
+                                color: white,
                                 fontSize: 17.34,
                                 fontWeight: FontWeight.w600),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ]),
+                    SizedBox(height: 25.03),
+
+                    //Sign in button
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 28.0),
+                      child: GestureDetector(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            )),
+                        child: Container(
+                          width: 366,
+                          height: 52.97,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: mainPurple,
+                              ),
+                              borderRadius: BorderRadius.circular(30.82)),
+                          child: Center(
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  fontFamily: 'Source Sans Pro',
+                                  color: mainPurple,
+                                  fontSize: 17.34,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+        ),
       ),
     );
   }
